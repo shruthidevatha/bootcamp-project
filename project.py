53f482e4f8b304e0b1102289c4c44b255de8966e
shruthidevatha <86914199+shruthidevatha@users.noreply.github.com>
2021-07-04 17:48:58 +0530

    uploaded the project file of python and network security bootcamp

---
+++ project.py
@@ -0,0 +1,12 @@
+Program to generate hashes of string data using algorithms from Hashlib
+import hashlib
+txt = input("enter text :")
+result1 = hashlib.md5(txt.encode())
+result2 = hashlib.sha1(txt.encode())
+result3 = hashlib.sha256(txt.encode())
+result4 = hashlib.sha512(txt.encode())
+print("the equivalent hashes are : " ,end= "\n")
+print(result1.hexdigest())
+print(result2.hexdigest())
+print(result3.hexdigest())
+print(result4.hexdigest())
