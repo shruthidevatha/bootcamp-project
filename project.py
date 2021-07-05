2780af379148254efeb36592b21d10ca1e109e98
shruthidevatha <86914199+shruthidevatha@users.noreply.github.com>
2021-07-05 09:31:16 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,9 @@
 # Program to generate hashes of string data using algorithms from Hashlib
 import hashlib
 txt = input("enter text :")
-result1 = hashlib.md5(txt.encode())
+result1 = hashlib.md5(txt.encode())  #generates MD5 of string data
 result2 = hashlib.sha1(txt.encode())
-result3 = hashlib.sha256(txt.encode())
+result3 = hashlib.sha256(txt.encode())   #hashes of string data using other algorithms of hashlib
 result4 = hashlib.sha512(txt.encode())
 print("the equivalent hashes are : " ,end= "\n")
 print(result1.hexdigest())
